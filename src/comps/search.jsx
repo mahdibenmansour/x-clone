d6784a2361a59c40a2100aa3e429518956ee2bd8
mahdi <benmansourmahdi12@gmail.com>
2024-12-13 20:18:59 +0100

    test

--- src/comps/search.jsx
+++ src/comps/search.jsx
@@ -5,6 +5,7 @@ export default function Search (){
         <div className="w-[100%] h-10  rounded-3xl bg-gray-600 bg-opacity-40 items-center content-center">
             <div className=" pl-4 flex ">
             <BiSearch size={20}/>
+            {/* testing */}
             <input placeholder="Search" type="text" className="pl-4 flex-grow outline-none bg-transparent"></input>
             </div>
         </div>
